a6348960b150db8a7242f1de488d809602796ab4
Fyoung1 <vladon-kazakov@mail.ru>
2025-05-20 22:37:00 +0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,4 @@ Route::get('/', function () {
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
 Route::post('/tasks/{taskId}/subtasks', [SubtaskController::class, 'store']);
-Route::put('/tasks/{task}', [TaskController::class, 'updateStatus']);
 Route::put('/subtasks/{subtask}', [SubtaskController::class, 'update']);
